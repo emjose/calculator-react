b7637750f249551832f147e52e8b2333def49c5c
emjose <emjose@gmail.com>
2021-02-24 16:22:41 -0500

    background test

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React from "react";
 
 import "./Button.css";
 
-const Button = (props) => {
-    return <div className="Button">AC</div>
-}
+const Button = ({ content, onButtonClick, type }) => {
+  return (
+    <div
+      className={`Button ${content === "0" ? "zero" : ""} ${type || ""}`}
+      onClick={onButtonClick(content)}
+    >
+      {content}
+    </div>
+  );
+};
 
 export default Button;
